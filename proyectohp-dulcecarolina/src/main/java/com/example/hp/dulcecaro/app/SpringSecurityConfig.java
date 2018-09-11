5618267948c57bc508c96b03ad91e2e4893edfa0
deeivid12 <deeivid12@gmail.com>
2018-09-11 00:30:33 -0300

    Configuracion inicial Spring Security
    Se hicieron las primeras configuraciones.
    Creacion de politicas de permisos de prueba.

---
+++ proyectohp-dulcecarolina/src/main/java/com/example/hp/dulcecaro/app/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.hp.dulcecaro.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception { //PARA VALIDAR PERMISOS!
+		
+		http.authorizeRequests().antMatchers("/", "/listar**", "/css/**", "/imagenes/**", "/js/**").permitAll() //RUTAS PUBLICAS!!
+		.antMatchers("/form**/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated();
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception { //build es el repo en donde se van a guardar los usuarios
+		
+		//ACA CONFIGURAR EL PASSWORD ENCODER + JPA!
+	}
+
+}

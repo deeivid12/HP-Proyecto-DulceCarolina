e6d9f979a8064228720bba633262ef59a5df0fc6
caro.blasich <caroblasich@gmail.com>
2018-09-11 21:26:01 -0300

    coment 2

--- proyectohp-dulcecarolina/src/main/java/com/example/hp/dulcecaro/app/controllers/ClienteController.java
+++ proyectohp-dulcecarolina/src/main/java/com/example/hp/dulcecaro/app/controllers/ClienteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.hp.dulcecaro.app.models.dao.IClienteDao;
 import com.example.hp.dulcecaro.app.models.entity.Cliente;
 //HOLA
+//comentario 2
 @Controller //marca y configura la clase como un controlador
 public class ClienteController {
 

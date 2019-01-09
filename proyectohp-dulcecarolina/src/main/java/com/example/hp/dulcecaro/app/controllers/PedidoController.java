65c5581ea0d98f926e2103ffec27b91cc5d4e19c
deeivid12 <deeivid12@gmail.com>
2019-01-09 01:49:19 -0300

    Creacion de carrito de compras
    Se crea primer versión de carro de compras. Se usa un pedido hardcodeado
    para prueba. Se crea boton de carrito de compra.

--- proyectohp-dulcecarolina/src/main/java/com/example/hp/dulcecaro/app/controllers/PedidoController.java
+++ proyectohp-dulcecarolina/src/main/java/com/example/hp/dulcecaro/app/controllers/PedidoController.java
@@ -1,14 +1,17 @@
 package com.example.hp.dulcecaro.app.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -18,8 +21,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.example.hp.dulcecaro.app.models.entity.ItemPedido;
 import com.example.hp.dulcecaro.app.models.entity.Pedido;
 import com.example.hp.dulcecaro.app.models.entity.Producto;
+import com.example.hp.dulcecaro.app.models.entity.Usuario;
 import com.example.hp.dulcecaro.app.models.service.IPedidoService;
 import com.example.hp.dulcecaro.app.models.service.IProductoService;
+import com.example.hp.dulcecaro.app.models.service.UsuarioServiceImpl;
 
 @Controller
 @RequestMapping("/pedido")
@@ -31,6 +36,9 @@ public class PedidoController {
 	
 	@Autowired
 	private IPedidoService pedidoService;
+	
+	@Autowired
+	private UsuarioServiceImpl uService;
 	
 	@GetMapping("/formPedido")
 //	public String crear(@PathVariable(value="clienteId") Long clienteId,
@@ -76,5 +84,47 @@ public class PedidoController {
 		status.setComplete(); //para finalaz el session attribute (notacion arriba)
 		flash.addFlashAttribute("success", "Pedido creado con éxito!");
 		return "redirect:/listarClientes";
+	}
+	
+	
+	
+	
+	
+	
+	@RequestMapping (value="/carroCompras", method=RequestMethod.GET)
+	public String carroCompras(Map<String, Object> model) {
+		
+		
+		Usuario uActual = new Usuario();
+		
+		//hardcodeo un pedido para el usuario actual
+		
+		Pedido p = new Pedido();
+		Producto pro = new Producto();
+		ItemPedido item = new ItemPedido();
+		pro.setNom("Producto");
+		pro.setDsc("Producto de prueba");
+		item.setProducto(pro); //en item cargo producto
+		item.setCantidad(1);//en item cargo cantidad de ese producto
+		item.setImporte(100.00);
+		
+		
+		List<ItemPedido> items = new ArrayList<ItemPedido>();
+		items.add(item);//guardo item en lista de items. por ahora esto es lo que voy a mandar a la vista.
+		p.setProductos(items);//guardo lista de items en pedido
+		
+		
+		
+		List<Pedido> pedidos = new ArrayList<Pedido>();
+		pedidos.add(p);
+
+		
+		//LO IMPORTANTE ES QUE EN EL CLIENTE SE GUARDEN PEDIDOS, CARGAR EL PEDIDO EN CURSO Y QUE MUESTRE TODOS LOS ITEMS!
+		
+		
+		uActual = uService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+		uActual.getCliente().setPedidos(pedidos);
+		model.put("items", items); //por ahora solo paso items para una prueba
+		return "carroCompras"; 
 	}
 }
